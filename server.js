f043a128feea76e260b587700f33f02e33b3631a
Sandip <sandip.tig@gmail.com>
2022-01-01 20:32:26 +0530

    2nd commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const dirName = path.resolve();
+app.use(express.static(path.join(dirName, '/frontend/build')));
+app.get('/login', (req, res) => {
+  res.send('Please login');
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(dirName, '/frontend/build/index.html'));
+});
+app.listen(5000, () => {
+  console.log('Server is running');
+});
